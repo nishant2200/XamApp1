77797fea30d36cfb5fcdfd068bcc47c1699a193e
nishant2200 <nishant2200@gmail.com>
2018-01-15 17:14:52 -0500

    testing change

--- XamApp1/MainActivity.cs
+++ XamApp1/MainActivity.cs
@@ -21,6 +21,7 @@ namespace XamApp1
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            //test
         }
     }
 }
